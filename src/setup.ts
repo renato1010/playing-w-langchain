b44927117cbefd3e068c66d80865f82ac58507eb
Renato <renatoperezc@gmail.com>
2024-05-13 08:15:13 -0600

    Add docs to ducuments folder

--- src/setup.ts
+++ src/setup.ts
@@ -1,21 +1,9 @@
-import path from 'node:path';
 import { pineconeIndexDocs } from '@/utils/pinecone-utils.js';
-import { TextLoader } from 'langchain/document_loaders/fs/text';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
-import { fileURLToPath } from 'node:url';
+import { getDocsFromDirectory } from './utils/directory-loader.js';
 
-const _dirname = path.dirname(fileURLToPath(import.meta.url));
-const docsFolderPath = path.join(_dirname, '../documents');
+
 export async function setup() {
-  // console.log({ docsFolderPath });
-  const loader = new DirectoryLoader(docsFolderPath, {
-    '.txt': (path) => new TextLoader(path),
-    '.md': (path) => new TextLoader(path),
-    '.pdf': (path) => new PDFLoader(path)
-  });
-
-  const docs = await loader.load();
+  const docs = await getDocsFromDirectory();
 
   try {
     await pineconeIndexDocs(docs);
--- src/utils/directory-loader.ts
+++ src/utils/directory-loader.ts
@@ -1,16 +1,21 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { Document } from '@langchain/core/documents';
 
-async function getDocsFromDirectory(dirPath: string): Promise<Document<Record<string, any>>[]> {
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+const docsFolderPath = path.join(_dirname, '../documents');
+async function getDocsFromDirectory(
+  dirPath = docsFolderPath
+): Promise<Document<Record<string, any>>[]> {
   const loader = new DirectoryLoader(dirPath, {
     '.txt': (path) => new TextLoader(path),
     '.md': (path) => new TextLoader(path),
     '.pdf': (path) => new PDFLoader(path)
   });
   const docs = await loader.load();
-  console.log({ docs });
   return docs;
 }
 
